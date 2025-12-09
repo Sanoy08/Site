5a58b2c54e3044aab23e1af0deb6bc411d0f4e3f
Sanoy Das <gamingheroyt2008@gmail.com>
2025-12-09 23:48:20 +0530

    4

--- android/app/src/main/java/com/bumbaskitchen/app/MainActivity.java
+++ android/app/src/main/java/com/bumbaskitchen/app/MainActivity.java
@@ -2,16 +2,20 @@ package com.bumbaskitchen.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import java.util.ArrayList;
-import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin; // ইম্পোর্ট
+
+// প্লাগিন ইম্পোর্ট
+import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin;
+import com.capacitorjs.plugins.app.AppPlugin;
+import com.getcapacitor.community.fcm.FCMPlugin; 
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // ম্যানুয়ালি প্লাগিন রেজিস্টার করা হচ্ছে
+        // ম্যানুয়ালি প্লাগিন রেজিস্টার করুন (Super কল করার আগে বা পরে, সাধারণত অটোমেটিক হয়, তবে ফোর্স করার জন্য এখানে দিলাম)
+        registerPlugin(AppPlugin.class);
         registerPlugin(PushNotificationsPlugin.class);
+        registerPlugin(FCMPlugin.class);
+
+        super.onCreate(savedInstanceState);
     }
 }

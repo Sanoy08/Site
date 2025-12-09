164c0779a517746d9e8b3c65a483511705318865
Sanoy Das <gamingheroyt2008@gmail.com>
2025-12-09 23:08:02 +0530

    3

--- android/app/src/main/java/com/bumbaskitchen/app/MainActivity.java
+++ android/app/src/main/java/com/bumbaskitchen/app/MainActivity.java
@@ -1,5 +1,17 @@
 package com.bumbaskitchen.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
+import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin; // ইম্পোর্ট
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // ম্যানুয়ালি প্লাগিন রেজিস্টার করা হচ্ছে
+        registerPlugin(PushNotificationsPlugin.class);
+    }
+}

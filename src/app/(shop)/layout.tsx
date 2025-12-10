a27a46ac0febe505ceb47f3567632739aaa5b69c
Sanoy Das <gamingheroyt2008@gmail.com>
2025-12-10 23:56:07 +0530

    Fix status bar overlap using safe-area CSS

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,3 +1,5 @@
+// src/app/(shop)/layout.tsx
+
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
 
@@ -8,22 +10,22 @@ export default function ShopLayout({
 }) {
   return (
     <div className="flex flex-col min-h-[100dvh]">
-      
-      {/* Fixed Status Bar Background */}
+
       <div 
-        className="fixed top-0 left-0 right-0 z-[100] bg-white"
+        className="fixed top-0 left-0 right-0 z-[9999] bg-white"
         style={{ height: 'env(safe-area-inset-top)' }} 
       />
 
-      {/* Content Wrapper */}
+
       <div className="flex-1 flex flex-col pt-[env(safe-area-inset-top)]">
         <Header />
-        <main className="flex-grow">{children}</main>
+        <main className="flex-grow relative z-0">
+          {children}
+        </main>
         <Footer />
       </div>
 
-      {/* Bottom Safe Area */}
-      <div className="pb-[env(safe-area-inset-bottom)]" />
+      <div className="pb-[env(safe-area-inset-bottom)] bg-white" />
     </div>
   );
 }

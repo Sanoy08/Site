dab0a700a7d06730c2791ce4dbc4c66da3525c8c
Sanoy Das <gamingheroyt2008@gmail.com>
2025-12-16 21:29:24 +0530

    old

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,25 +5,17 @@ const config: CapacitorConfig = {
   appName: "Bumba's Kitchen",
   webDir: 'public',
   server: {
-    url: 'https://www.bumbaskitchen.app',
+    url: 'https://www.bumbaskitchen.app', // ★ এই লাইনটি আবার চালু করুন
     cleartext: true,
     allowNavigation: [
       'www.bumbaskitchen.app',
       'bumbaskitchen.app',
-      '*.bumbaskitchen.app',
-      'accounts.google.com' // Allow Google Auth navigation
+      '*.bumbaskitchen.app'
     ]
   },
   plugins: {
     PushNotifications: {
       presentationOptions: ["badge", "sound", "alert"]
-    },
-    // [ADD THIS SECTION]
-    GoogleAuth: {
-      scopes: ['profile', 'email'],
-      // IMPORTANT: Use your "Web Client ID" here, not Android/iOS ID
-      serverClientId: process.env.GOOGLE_CLIENT_ID || '1071253759706-k2sdmhlv0bao4osb3m6fnnsemvftigq9.apps.googleusercontent.com',
-      forceCodeForRefreshToken: true,
     }
   }
 };

2d0a7c7bab847619a91604f75185871373304953
Sanoy Das <gamingheroyt2008@gmail.com>
2025-12-20 23:06:44 +0530

    8

--- android/app/src/main/java/com/bumbaskitchen/app/MainActivity.java
+++ android/app/src/main/java/com/bumbaskitchen/app/MainActivity.java
@@ -3,20 +3,18 @@ package com.bumbaskitchen.app;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-// অন্য প্লাগিনগুলোর সাথে এই নতুন ইম্পোর্টটি যোগ করুন
+// ১. এই ইম্পোর্টগুলো যোগ করুন
 import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin;
 import com.capacitorjs.plugins.app.AppPlugin;
-import com.getcapacitor.community.fcm.FCMPlugin;
-import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthentication; // ★ এটি মিসিং ছিল
+import com.getcapacitor.community.fcm.FCMPlugin; 
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // এখানে নতুন প্লাগিনটি রেজিস্টার করুন
+        // ২. এই লাইনগুলো যোগ করুন (Super এর আগে বা পরে)
         registerPlugin(AppPlugin.class);
         registerPlugin(PushNotificationsPlugin.class);
         registerPlugin(FCMPlugin.class);
-        registerPlugin(FirebaseAuthentication.class); // ★ এই লাইনটি যোগ করুন
 
         super.onCreate(savedInstanceState);
     }

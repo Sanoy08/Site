f51bc3180dd8c59f6472b6d2accdfe0f6493aa23
Sanoy Das <gamingheroyt2008@gmail.com>
2026-05-15 22:53:06 +0530

    3 (v1.0.91)

--- src/components/StatusBarLogic.tsx
+++ src/components/StatusBarLogic.tsx
@@ -1,5 +1,4 @@
-// src/Components/StatusBarLogic.tsx
-
+// src/components/StatusBarLogic.tsx
 'use client';
 
 import { useEffect } from 'react';
@@ -11,9 +10,13 @@ export default function StatusBarLogic() {
     if (Capacitor.isNativePlatform()) {
       const initStatusBar = async () => {
         try {
-
+          // ১. অ্যাপকে নচ এরিয়া পর্যন্ত ফোর্স করা
           await StatusBar.setOverlaysWebView({ overlay: true });
 
+          // ২. ★ স্ট্যাটাস বারের ব্যাকগ্রাউন্ড পুরোপুরি ট্রান্সপারেন্ট (স্বচ্ছ) করা ★
+          await StatusBar.setBackgroundColor({ color: '#00000000' });
+
+          // ৩. আইকনগুলো যেন বোঝা যায় তার জন্য
           await StatusBar.setStyle({ style: Style.Dark });
           
         } catch (e) {
